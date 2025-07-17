53eb60e2cc321b02c5de1c3714d508d5846d84a6
Lawal Giyath <lawalgiyath200716@gmail.com>
2025-07-17 16:41:43 +0000

    this si the ncorrect nigerian suicide prevention site https://nigeriasui

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Firebase Studio
-
-This is a NextJS starter in Firebase Studio.
-
-To get started, take a look at src/app/page.tsx.
+realme by:
+  DR Mrs Jaiyeoba
+  Lawal Giyath
+  Jaiyeoba Tomilola
+  Adekunle Abdulazeez
--- src/components/resource-directory.tsx
+++ src/components/resource-directory.tsx
@@ -24,7 +24,7 @@ const resources = [
     name: 'Nigeria Suicide Prevention Initiative',
     description: 'Provides suicide prevention services and counseling for individuals in crisis.',
     phone: '+2348062106493',
-    website: 'nspi.com.ng',
+    website: 'nigeriasuicideprevention.com',
   },
 ];
 
