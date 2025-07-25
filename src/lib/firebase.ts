d1e2193bc61b666dee5d1dc2f820fd9aad71726d
Lawal Giyath <lawalgiyath200716@gmail.com>
2025-07-25 12:13:26 +0000

    FirebaseError: Firebase: Error (auth/popup-closed-by-user).     at creat

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,23 +16,30 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 // Enable offline persistence
-enableIndexedDbPersistence(db)
-  .catch((err) => {
-    if (err.code == 'failed-precondition') {
-      // Multiple tabs open, persistence can only be enabled
-      // in one tab at a time.
-      console.warn("Firestore persistence failed: multiple tabs open.");
-    } else if (err.code == 'unimplemented') {
-      // The current browser does not support all of the
-      // features required to enable persistence
-      console.warn("Firestore persistence is not supported in this browser.");
-    }
-  });
+try {
+  enableIndexedDbPersistence(db)
+} catch (err: any) {
+  if (err.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a time.
+    console.warn("Firestore persistence failed: multiple tabs open.");
+  } else if (err.code == 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+    console.warn("Firestore persistence is not supported in this browser.");
+  }
+}
 
 
 export { app, auth, db };
